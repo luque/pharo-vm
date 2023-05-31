6ae97685ae2f791982c8312732ef5e6b8cb9cf75
Rafael Luque <rafael.luque.leiva@gmail.com>
2023-05-31 12:13:11 +0200

    Checking if Cairo library is present in the system before download it

--- cmake/importCairo.cmake
+++ cmake/importCairo.cmake
@@ -1,6 +1,15 @@
-if (BUILD_BUNDLE)
+function(find_system_Cairo)
+  message(STATUS "Looking for Cairo in the system")
+  find_package(Cairo)
+  if(Cairo_FOUND)
+    add_dependencies(${VM_LIBRARY_NAME} Cairo::Cairo)
+  else()
+    message(STATUS "Cairo not found.")
+  endif()
+  set(Cairo_FOUND ${Cairo_FOUND} PARENT_SCOPE)
+endfunction()
 
-  # Cairo does not support building on CMake
+function(download_Cairo)
   if (WIN)
     If(${CMAKE_SYSTEM_PROCESSOR} MATCHES "ARM64")
       add_third_party_dependency("pixman-0.40.0")
@@ -32,5 +41,41 @@ if (BUILD_BUNDLE)
       add_third_party_dependency("cairo-1.17.4")
       add_third_party_dependency("libpng-1.6.37")
     endif()  
+  endif()
+endif()
+endFunction()
+
+function(build_Cairo)
+  message(STATUS "Building Cairo")
+
+endfunction()
+
+if (BUILD_BUNDLE)
+  #Only get Freetype if required
+  if(PHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES)
+    #Download Freetype binaries directly
+    download_Freetype()
+  else()
+    #Look for Freetype in the system, then build or download if possible
+    find_system_Freetype()
+    if(NOT Freetype_FOUND)
+        build_Freetype()
+    endif()
+  endif()
+endif()
+
+
+
+if (BUILD_BUNDLE)
+  #Only get Cairo if required
+  if(PHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES)
+    #Download Cairo binaries directly
+    download_Cairo()
+  else()
+    #Look for Cairo in the system, then build or download if possible
+    find_system_Cairo()
+    if(NOT Cairo_FOUND)
+        build_Cairo()
+    endif()
   endif()
 endif()
